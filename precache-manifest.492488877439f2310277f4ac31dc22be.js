a541ca385f7a9c523fbfee3f2214661397ffb75b
Ernesto Briceño <ejbcode@gmail.com>
2020-08-23 21:30:12 -0400

    Updates

---
+++ precache-manifest.492488877439f2310277f4ac31dc22be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45378237d10279d79722ec84b180c69",
+    "url": "/react_redux_basic/index.html"
+  },
+  {
+    "revision": "18ff754380313db5ee72",
+    "url": "/react_redux_basic/static/css/main.76af7d1c.chunk.css"
+  },
+  {
+    "revision": "5736e09b9a163c52163a",
+    "url": "/react_redux_basic/static/js/2.fff5fdf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_redux_basic/static/js/2.fff5fdf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ff754380313db5ee72",
+    "url": "/react_redux_basic/static/js/main.3f560870.chunk.js"
+  },
+  {
+    "revision": "6c5ba2392c8322ccb37a",
+    "url": "/react_redux_basic/static/js/runtime-main.d8cbdb0a.js"
+  }
+]);
